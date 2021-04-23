a045070393db437ee68501ef020935a15260ad5d
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-04-23 12:02:22 +0300

    Add tests for health check

---
+++ test/health.check.test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import HealthCheck from '../lib/health.check.controller.js';
+
+describe('Health Check', function () {
+  let response = null;
+
+  before(async function () {
+    response = await HealthCheck.ping();
+  });
+
+  it('should return http status code 200', async function () {
+    expect(response.status).to.eq(201);
+    expect(response.statusText).to.eq('Created');
+  });
+
+  it('should return content-type header', async function () {
+    expect(response.headers['content-type']).to.eq('application/json; charset=utf-8');
+  });
+
+  it('should return valid response body', async function () {
+    expect(response.data).to.eq('Created');
+  });
+});
